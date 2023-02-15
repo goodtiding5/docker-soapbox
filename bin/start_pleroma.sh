9a7da20d925be198197c07e09f1b12e83a6e92e7
Kenneth Zhao <ken@epenguin.com>
2023-02-15 11:57:55 -0800

    upgrade to soapbox v3.2.0 release

--- bin/start_pleroma.sh
+++ bin/start_pleroma.sh
@@ -5,6 +5,9 @@ set -e
 WORKDIR=/opt/pleroma
 DATADIR=/var/lib/pleroma
 
+#SOAPBOX="https://gitlab.com/soapbox-pub/soapbox/-/jobs/artifacts/develop/download?job=build-production"
+SOAPBOX="https://gitlab.com/soapbox-pub/soapbox/-/jobs/artifacts/v3.2.0/download?job=build-production"
+
 echo "-- Waiting for database..."
 while ! pg_isready -U ${DB_USER:-pleroma} -d postgres://${DB_HOST:-db}:5432/${DB_NAME:-pleroma} -t 1; do
     sleep 1s
@@ -14,7 +17,7 @@ done
 echo "-- Updating FE..."
 
 # Download the build.
-curl -L "https://gitlab.com/soapbox-pub/soapbox/-/jobs/artifacts/develop/download?job=build-production" -o /tmp/soapbox-fe.zip
+curl -L "$SOAPBOX"  -o /tmp/soapbox-fe.zip
 
 # Remove all the current Soapbox build in Pleroma's instance directory.
 rm -fR ${DATADIR}/static/packs
